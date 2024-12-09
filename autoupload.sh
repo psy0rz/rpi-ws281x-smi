17c5b1650c1a7ee59af0ca3651d22c828958f0b7
Edwin Eefting <edwin@datux.nl>
2024-12-09 21:57:58 +0100

    bugfix alpha bending

--- autoupload.sh
+++ autoupload.sh
@@ -7,5 +7,5 @@ IP=$1
 STUFF="*.js *.cpp smi *.json CMakeLists.txt *.c *.h"
 TEST="node test.js"
 
-find $STUFF|entr -r -s "rsync -avx $STUFF root@$IP:rpi-ws281x-smi && ssh root@$IP 'killall node; cd rpi-ws281x-smi; cmake-js compile; $TEST '"
-
+#find $STUFF|entr -r -s "rsync -avx $STUFF root@$IP:rpi-ws281x-smi && ssh root@$IP 'killall node; cd rpi-ws281x-smi; cmake-js compile; $TEST '"
+find $STUFF | entr -r -s "rsync -avx $STUFF root@$IP:rpi-ws281x-smi && ssh root@$IP 'cd ledder && rm -rf node_modules/rpi-ws281x-smi && npm install ../rpi-ws281x-smi && node ledder/server/server.js'"
--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 #include "smileds.h"
 
 void ledsInit(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
+  Napi::Env env = info.Env();
 
-    int ledCount = info[0].As<Napi::Number>();
-    if (!leds_init(ledCount))
-    {
-        Napi::TypeError::New(env, "Failed").ThrowAsJavaScriptException();
-    }
+  int ledCount = info[0].As<Napi::Number>();
+  if (!leds_init(ledCount)) {
+    Napi::TypeError::New(env, "Failed").ThrowAsJavaScriptException();
+  }
 }
 
 /**
@@ -17,41 +16,38 @@ void ledsInit(const Napi::CallbackInfo& info) {
  * @param info
  */
 void ledsSetPixel(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
+  Napi::Env env = info.Env();
 
-    int channel = info[0].As<Napi::Number>();
-    int pixel = info[1].As<Napi::Number>();
+  int channel = info[0].As<Napi::Number>();
+  int pixel = info[1].As<Napi::Number>();
 
-    color_t color;
+  color_t color;
 
-    color.component.r=(uint32_t )info[2].As<Napi::Number>();
-    color.component.g=(uint32_t )info[3].As<Napi::Number>();
-    color.component.b=(uint32_t )info[4].As<Napi::Number>();
-    color.component.a=(uint32_t )(((float )(info[5].As<Napi::Number>()))*255); //every extra parameter takes about 10fps in our 16*256 led test.
+  color.component.r = (uint32_t)info[2].As<Napi::Number>();
+  color.component.g = (uint32_t)info[3].As<Napi::Number>();
+  color.component.b = (uint32_t)info[4].As<Napi::Number>();
+  color.component.a = (uint32_t)(((float)(info[5].As<Napi::Number>())) *
+                                 255);  // every extra parameter takes about
+                                        // 10fps in our 16*256 led test.
 
-    leds_set_pixel(channel, pixel, color);
+  leds_set_pixel(channel, pixel, color);
 }
 
 void ledsSend(const Napi::CallbackInfo& info) {
-    leds_send();
+  leds_send();
 }
 
 void ledsClear(const Napi::CallbackInfo& info) {
-    leds_clear();
+  leds_clear();
 }
-
-
-
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports["init"] = Napi::Function::New(env, ledsInit);
-    exports["send"] = Napi::Function::New(env, ledsSend);
-    exports["clear"] = Napi::Function::New(env, ledsClear);
-    exports["setPixel"] = Napi::Function::New(env, ledsSetPixel);
+  exports["init"] = Napi::Function::New(env, ledsInit);
+  exports["send"] = Napi::Function::New(env, ledsSend);
+  exports["clear"] = Napi::Function::New(env, ledsClear);
+  exports["setPixel"] = Napi::Function::New(env, ledsSetPixel);
 
   return exports;
-
 }
-
 
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
