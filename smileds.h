00895f3601b14bcfa0c958a83670a95b9decbad9
Edwin Eefting <edwin@datux.nl>
2025-01-03 20:35:38 +0100

    formatting only

--- smileds.h
+++ smileds.h
@@ -8,26 +8,23 @@
 #define RPI_WS281X_SMI_SMILEDS_H
 
 union color_t {
-    uint32_t packed; //packed presentation
-    struct  // component-wise representation
-    {
-        uint8_t b;
-        uint8_t r;
-        uint8_t g;
-        uint8_t a;
-    } component ;
+  uint32_t packed;  // packed presentation
+  struct            // component-wise representation
+  {
+    uint8_t b;
+    uint8_t r;
+    uint8_t g;
+    uint8_t a;
+  } component;
 };
 
-
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-
 
 bool leds_init(int init_led_count);
 
-   void leds_set_pixel(uint8_t  channel, uint16_t  pixel,  union color_t   color);
+void leds_set_pixel(uint8_t channel, uint16_t pixel, union color_t color);
 
 void leds_send();
 
@@ -35,10 +32,8 @@ void leds_clear();
 
 void test();
 
-
 #ifdef __cplusplus
 }
 #endif
 
-
-#endif //RPI_WS281X_SMI_SMILEDS_H
+#endif  // RPI_WS281X_SMI_SMILEDS_H
